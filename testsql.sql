db002f087ed259510fc789cbc70c85cc83b9ee1a
kiransimhadri754 <157337219+kiransimhadri754@users.noreply.github.com>
2024-09-11 18:02:49 +0530

    Update testsql.sql

--- testsql.sql
+++ testsql.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE  rest_db
-DROP TABLE IF EXISTS report_access_log;
-CREATE TABLE report_access_log (
- id varchar(255) NOT NULL,
- report_name varchar (64),
- parameters varchar (256),
- user_name varchar (64),
- generated_timestamp date,
- PRIMARY KEY (id)
+CREATE DATABASE 'rest_db';
+USE `rest_db`;
+
+DROP TABLE IF EXISTS `report_access_log`;
+CREATE TABLE `report_access_log` (
+ `id` varchar(255) NOT NULL,
+ `report_name` varchar (64),
+ `parameters` varchar (256),
+ `user_name` varchar (64),
+ `generated_timestamp` date,
+ PRIMARY KEY (`id`)
 );
